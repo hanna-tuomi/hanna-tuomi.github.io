99caab0c2225fecde5e6f425bad406bb2316ca90
hanna-tuomi <hanna.tuomi@gmail.com>
2020-08-22 03:24:01 -0400

    Add templates and routes

--- app/routes.py
+++ app/routes.py
@@ -3,13 +3,4 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-'''
-<html>
-    <head>
-        <title>htuomi</title>
-    </head>
-    <body>
-        <h1>senate-vote project</h1>
-        <a href='https://github.com/hanna-tuomi/senate-votes'>Git Repo</a>
-    </body>
-</html>'''
+    return render_template('index.html')
